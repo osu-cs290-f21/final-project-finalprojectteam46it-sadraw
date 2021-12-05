c8148680479c761be3d2ead60d75514ae2d3334f
Gavin Pham <phamga@oregonstate.edu>
2021-12-04 20:30:13 -0800

    basic changes to handlebars html, css, js, but no inclusion of canvas yet

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
-# ADD STUFF TO THIS
+document.getElementById("post-button").addEventListener("click", toggleModal)
+document.getElementById("modal-close").addEventListener("click", toggleModal)
+document.getElementById("modal-cancel").addEventListener("click", toggleModal)
+
+function toggleModal() {
+  /*  document.getElementById("post-text-input").value = ""
+    document.getElementById("post-photo-input").value = ""
+    document.getElementById("post-price-input").value = ""
+    document.getElementById("post-city-input").value = ""
+    document.getElementById("post-condition-new").checked = true */
+    document.getElementById("modal-backdrop").classList.toggle("hidden")
+    document.getElementById("post-something-modal").classList.toggle("hidden")
+}
