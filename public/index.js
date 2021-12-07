b8e0cdebff1b5c6fe418a937faf0b73db1f18fcf
MahmoudFakhry <74266851+MahmoudFakhry@users.noreply.github.com>
2021-12-07 08:37:02 -0800

    reverted to old index.js
    This is in order to keep this index.js version separate from a different branch.

--- public/index.js
+++ public/index.js
@@ -1,16 +1,9 @@
 document.getElementById("post-button").addEventListener("click", toggleModal)
-document.getElementById("modal-close").addEventListener("click", toggleModal)
+document.getElementById("modal-close").addEventListener("click", checkToggleModal)
 document.getElementById("modal-cancel").addEventListener("click", toggleModal)
 document.getElementById("modal-accept").addEventListener("click", modalAccept)
 
 var yourPosts = [];
-
-let button_clicks = [
-    {
-        "clicked_likes": 0,
-        "clicked_dislikes": 0
-    }
-]
 
 const canvas = document.getElementById("canvas");
 const clearEl = document.getElementById("clear");
@@ -20,9 +13,6 @@ let isPressed = false;
 let color = "black";
 let x = undefined;
 let y = undefined;
-
-var i_d; // To keep track of element position
-
 
 canvas.addEventListener("mousedown", (e) => {
     isPressed = true;
@@ -39,6 +29,7 @@ canvas.addEventListener("mouseup", (e) => {
 });
 
 canvas.addEventListener("mousemove", (e) => {
+
     if (isPressed) {
         const x2 = e.offsetX;
         const y2 = e.offsetY;
@@ -49,6 +40,13 @@ canvas.addEventListener("mousemove", (e) => {
         y = y2;
     }
 });
+
+// Added: ###
+function erase() {
+    // Can make anonymous function
+    color = "white";
+}
+
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
@@ -68,191 +66,91 @@ clearEl.addEventListener("click", () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 function modalAccept() { /*Old way to do html insert I'll do both but somethings need to be done before hand to check*/
-    var canvasData = canvas.toDataURL("image/png");
-    console.log(canvasData)
-    var newPost = document.createElement('div')
-    newPost.classList.add('post')
+  var canvasData = canvas.toDataURL("image/png");
+  console.log(canvasData)
+  var newPost = document.createElement('div')
+  newPost.classList.add('post')
 
-    var postContentsDiv = document.createElement('div')
-    postContentsDiv.classList.add('post-contents')
-    newPost.appendChild(postContentsDiv)
+  var postContentsDiv = document.createElement('div')
+  postContentsDiv.classList.add('post-contents')
+  newPost.appendChild(postContentsDiv)
 
-    var postImg = document.createElement('div')
-    postImg.classList.add('post-image-container')
-    postContentsDiv.appendChild(postImg)
+  var postImg = document.createElement('div')
+  postImg.classList.add('post-image-container')
+  postContentsDiv.appendChild(postImg)
 
-    var photoImg = document.createElement('img')
-    photoImg.src = canvasData
-    postImg.appendChild(photoImg)
+  var photoImg = document.createElement('img')
+  photoImg.src = canvasData
+  postImg.appendChild(photoImg)
 
-    var postTextContents = document.createElement('div')
-    postTextContents.classList.add('post-text-container')
-    postContentsDiv.appendChild(postTextContents)
+  var postTextContents = document.createElement('div')
+  postTextContents.classList.add('post-text-container')
+  postContentsDiv.appendChild(postTextContents)
 
-    var postText = document.createElement('div')
-    postText.classList.add('post-text')
-    postText.textContent = document.getElementById("post-text-input").value
-    postTextContents.appendChild(postText)
+  var postText = document.createElement('div')
+  postText.classList.add('post-text')
+  postText.textContent = document.getElementById("post-text-input").value
+  postTextContents.appendChild(postText)
 
-    var postInfoContainer = document.createElement('div')
-    postInfoContainer.classList.add('post-info-container')
-    postTextContents.appendChild(postInfoContainer)
+  var postInfoContainer = document.createElement('div')
+  postInfoContainer.classList.add('post-username')
+  postTextContents.appendChild(postInfoContainer)
 
-    var postUsername = document.createElement('span')
-    postUsername.classList.add('post-username')
-    postUsername.textContent = document.getElementById("username-input").value
-    postInfoContainer.appendChild(postUsername)
+  var postUsername = document.createElement('span')
+  postUsername.classList.add('post-username')
+  postUsername.textContent = document.getElementById("username-input").value
+  postInfoContainer.appendChild(postUsername)
 
-    var likeButton = document.createElement('button')
-    likeButton.classList.add('like-button')
-    likeButton.setAttribute('id', 'like-button')
-    likeButton.textContent = "Likes: 0"
-    postInfoContainer.appendChild(likeButton)
+  var likeButton = document.createElement('button')
+  likeButton.classList.add('like-button')
+  likeButton.textContent = 0
+  postInfoContainer.appendChild(likeButton)
 
-    var dislikeButton = document.createElement('button')
-    dislikeButton.classList.add('dislike-button')
-    dislikeButton.setAttribute('id', 'dislike-button')
-    dislikeButton.textContent = "Dislikes: 0"
-    postInfoContainer.appendChild(dislikeButton)
+  var dislikeButton = document.createElement('button')
+  dislikeButton.classList.add('dislike-button')
+  dislikeButton.textContent = 0
+  postInfoContainer.appendChild(dislikeButton)
 
-    var postDate = document.createElement('span')
-    postDate.classList.add('post-date')
-    postDate.textContent = document.getElementById("post-date-input").value
-    postInfoContainer.appendChild(postDate)
+  var postDate = document.createElement('span')
+  postDate.classList.add('post-date')
+  postDate.textContent = document.getElementById("post-date-input").value
+  postInfoContainer.appendChild(postDate)
 
 
-    yourPosts.push(newPost)
-    document.getElementById("posts").appendChild(newPost)
+  yourPosts.push(newPost)
+  document.getElementById("posts").appendChild(newPost)
 
-    var dislikeButtons = document.getElementsByClassName("dislike-button")
-    console.log(dislikeButtons)
-    /*
-    for (var i = 0; i < dislikeButtons.length; i++) {
-        dislikeButtons[i].addEventListener('click', addDislike)
-    } */
-    for (var i = 0; i < dislikeButtons.length; i++) {
-        //console.log("dislikeButtons.length", dislikeButtons.length)
-        i_d = i;
-        console.log("Step1", i_d)
-        console.log("Step2", i)
-        dislikeButtons[i_d].addEventListener('click', addDislike)
-    }
 
-    var obj = {
-        "clicked_likes": 0,
-        "clicked_dislikes": 0
-    }
-    button_clicks.push(obj);
+  toggleModal()
+/* uses template
+var context = {
+  imageSrc: canvasData,
+  imageText: document.getElementById("post-text-input").value,
+  username: document.getElementById("username-input").value,
+  likes: 0,
+  dislikes: 0,
+  date: document.getElementById("post-date-input").value
+}
+var newPost = Handlebars.templates.post(context)
+var postsSection = document.getElementById('posts');
+postsSection.insertAdjacentHTML('beforeend',newPost)
+toggleModal()
+*/
 
-    console.log("length of arr", button_clicks.length)
-
-    toggleModal()
-    /* uses template
-    var context = {
-      imageSrc: canvasData,
-      imageText: document.getElementById("post-text-input").value,
-      username: document.getElementById("username-input").value,
-      likes: 0,
-      dislikes: 0,
-      date: document.getElementById("post-date-input").value
-    }
-    var newPost = Handlebars.templates.post(context)
-    var postsSection = document.getElementById('posts');
-    postsSection.insertAdjacentHTML('beforeend',newPost)
-    toggleModal()
-    */
 
 }
 
-/*
-function addDislike(event){
- var dislike = (event.target)
- console.log(dislike.textContent)
- var numDislike = dislike.textContent.slice(-1)
- console.log(+numDislike + +1)
- dislike.textContent = "Dislike: " + (+numDislike + +1)
- 
-} */
-
-var dislikeButtons = document.getElementsByClassName("dislike-button")
-console.log(dislikeButtons)
-for (var i = 0; i < dislikeButtons.length; i++) {
-    //console.log("dislikeButtons.length", dislikeButtons.length)
-    i_d = i;
-    dislikeButtons[i_d].addEventListener('click', addDislike)
-}
-
-
-
-////
-function addDislike(event) {
-    var dislikeButtons = document.getElementsByClassName("dislike-button")
-    console.log("dislikeButtons.length", dislikeButtons.length)
-
-
-    console.log("i_d", i_d); // Remember: For loop adds one at end, so subtract one for correct element position.
-    var dislike = (event.target).textContent;
-    console.log(dislike)
-
-    var button_txt = "Dislikes";
-    for (var i = 0; i < 8; i++) {
-        if (button_txt[i] !== dislike[i]) {
-            return; // Checks if dislike button; else exit function
-        }
-    }
-
-    // Loop to get the number after the "Like: " in the like button.
-    var str_dislike = String(dislike);
-    var str_num = "";
-    for (var i = str_dislike.length - 1; i >= 0; i--) {
-        if (str_dislike[i] == " ") {
-            break;
-        }
-        str_num += str_dislike[i];
-    }
-    var num_dislikes = "";
-    for (var i = str_num.length - 1; i >= 0; i--) {
-        num_dislikes += str_num[i];
-    }
-
-    // Check to see if any dislikes previously added using "button_clicks" object and element position (from "i_d" element position in for loop).
-    var num_added_dislikes = button_clicks[i_d].clicked_dislikes;
-    console.log(button_clicks[i_d].clicked_dislikes);
-    if (num_added_dislikes == 0) { // No previous dislikes
-        num_dislikes++;
-        (event.target).textContent = "Dislikes: " + num_dislikes;
-        // Increment dislikes in object:
-        button_clicks[i_d].clicked_dislikes++;
-        // Now, check to see if there are any likes we placed on the post
-        var num_added_likes = button_clicks[i_d].clicked_likes;
-        if (num_added_likes == 1) {
-            //
-            // Come back to this
-            //
-        }
-    } else { // There is a previous dislike selected; we need to remove it
-        num_dislikes--;
-        (event.target).textContent = "Dislikes: " + num_dislikes;
-        // Decrement dislikes in object:
-        button_clicks[i_d].clicked_dislikes--;
+function checkToggleModal() { 
+    if (confirm("Are you sure you want to discard your work? This cannot be undone.")) {
+        toggleModal();
     }
 }
-
-/*
-function addDislike() {
-   var dislike = document.getElementById("like-button").textContent;
-   var str_dislike = String(dislike);
-} */
-
-
-
 
 function toggleModal() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     document.getElementById("post-date-input").value = ""
     document.getElementById("post-text-input").value = ""
+
     document.getElementById("modal-backdrop").classList.toggle("hidden")
     document.getElementById("post-something-modal").classList.toggle("hidden")
 }
-
-
